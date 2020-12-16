37f95137a22728478f00265cc9f70a528d9e2a9f
Bálint Bak <balint.bak@stud.uni-corvinus.hu>
2020-12-16 16:52:03 +0100

    New Classes

---
+++ IRF_Project/IRF_Project/Entities/Ball.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace IRF_Project.Entities
+{
+    public class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 30;
+            Height = 30;
+
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Black), 0, 0, Width, Height);
+            g.FillEllipse(new SolidBrush(Color.White), 0, 0, 28, 28);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
