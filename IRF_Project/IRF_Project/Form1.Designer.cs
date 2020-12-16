1d0cd3f140cf82bb9d55b77036655b096e16e061
Bálint Bak <balint.bak@stud.uni-corvinus.hu>
2020-12-16 16:23:46 +0100

    Csv write and buttons

--- IRF_Project/IRF_Project/Form1.Designer.cs
+++ IRF_Project/IRF_Project/Form1.Designer.cs
@@ -28,22 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dGVAttributes = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonWrite = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dGVAttributes)).BeginInit();
+            this.Reload = new System.Windows.Forms.Button();
+            this.btnForwards = new System.Windows.Forms.Button();
+            this.btnMidfilders = new System.Windows.Forms.Button();
+            this.btnDefenders = new System.Windows.Forms.Button();
+            this.btnGoalkeepers = new System.Windows.Forms.Button();
+            this.btnAnimation = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // dGVAttributes
+            // dataGridView1
             // 
-            this.dGVAttributes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dGVAttributes.Location = new System.Drawing.Point(12, 12);
-            this.dGVAttributes.Name = "dGVAttributes";
-            this.dGVAttributes.Size = new System.Drawing.Size(596, 395);
-            this.dGVAttributes.TabIndex = 0;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(577, 320);
+            this.dataGridView1.TabIndex = 0;
             // 
             // buttonWrite
             // 
-            this.buttonWrite.Location = new System.Drawing.Point(656, 31);
+            this.buttonWrite.Location = new System.Drawing.Point(656, 103);
             this.buttonWrite.Name = "buttonWrite";
             this.buttonWrite.Size = new System.Drawing.Size(99, 34);
             this.buttonWrite.TabIndex = 1;
@@ -51,24 +57,96 @@
             this.buttonWrite.UseVisualStyleBackColor = true;
             this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
             // 
+            // Reload
+            // 
+            this.Reload.Location = new System.Drawing.Point(656, 46);
+            this.Reload.Name = "Reload";
+            this.Reload.Size = new System.Drawing.Size(99, 34);
+            this.Reload.TabIndex = 2;
+            this.Reload.Text = "Reload";
+            this.Reload.UseVisualStyleBackColor = true;
+            this.Reload.Click += new System.EventHandler(this.Reload_Click);
+            // 
+            // btnForwards
+            // 
+            this.btnForwards.Location = new System.Drawing.Point(12, 364);
+            this.btnForwards.Name = "btnForwards";
+            this.btnForwards.Size = new System.Drawing.Size(99, 34);
+            this.btnForwards.TabIndex = 3;
+            this.btnForwards.Text = "Forwards";
+            this.btnForwards.UseVisualStyleBackColor = true;
+            this.btnForwards.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMidfilders
+            // 
+            this.btnMidfilders.Location = new System.Drawing.Point(151, 364);
+            this.btnMidfilders.Name = "btnMidfilders";
+            this.btnMidfilders.Size = new System.Drawing.Size(99, 34);
+            this.btnMidfilders.TabIndex = 4;
+            this.btnMidfilders.Text = "Midfilders";
+            this.btnMidfilders.UseVisualStyleBackColor = true;
+            this.btnMidfilders.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnDefenders
+            // 
+            this.btnDefenders.Location = new System.Drawing.Point(301, 364);
+            this.btnDefenders.Name = "btnDefenders";
+            this.btnDefenders.Size = new System.Drawing.Size(99, 34);
+            this.btnDefenders.TabIndex = 5;
+            this.btnDefenders.Text = "Defenders";
+            this.btnDefenders.UseVisualStyleBackColor = true;
+            this.btnDefenders.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnGoalkeepers
+            // 
+            this.btnGoalkeepers.Location = new System.Drawing.Point(446, 364);
+            this.btnGoalkeepers.Name = "btnGoalkeepers";
+            this.btnGoalkeepers.Size = new System.Drawing.Size(99, 34);
+            this.btnGoalkeepers.TabIndex = 6;
+            this.btnGoalkeepers.Text = "Goal-Keepers";
+            this.btnGoalkeepers.UseVisualStyleBackColor = true;
+            this.btnGoalkeepers.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnAnimation
+            // 
+            this.btnAnimation.Location = new System.Drawing.Point(643, 338);
+            this.btnAnimation.Name = "btnAnimation";
+            this.btnAnimation.Size = new System.Drawing.Size(119, 73);
+            this.btnAnimation.TabIndex = 7;
+            this.btnAnimation.Text = "Animation";
+            this.btnAnimation.UseVisualStyleBackColor = true;
+            this.btnAnimation.Click += new System.EventHandler(this.btnAnimation_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAnimation);
+            this.Controls.Add(this.btnGoalkeepers);
+            this.Controls.Add(this.btnDefenders);
+            this.Controls.Add(this.btnMidfilders);
+            this.Controls.Add(this.btnForwards);
+            this.Controls.Add(this.Reload);
             this.Controls.Add(this.buttonWrite);
-            this.Controls.Add(this.dGVAttributes);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dGVAttributes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dGVAttributes;
+        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonWrite;
+        private System.Windows.Forms.Button Reload;
+        private System.Windows.Forms.Button btnForwards;
+        private System.Windows.Forms.Button btnMidfilders;
+        private System.Windows.Forms.Button btnDefenders;
+        private System.Windows.Forms.Button btnGoalkeepers;
+        private System.Windows.Forms.Button btnAnimation;
     }
 }
 
--- IRF_Project/IRF_Project/Form1.cs
+++ IRF_Project/IRF_Project/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,15 +15,16 @@ namespace IRF_Project
     public partial class Form1 : Form
     {
         BindingList<players> Chelsea = new BindingList<players>();
+        XmlDocument xml = new XmlDocument();
 
         public Form1()
         {
             InitializeComponent();
 
-            XmlDocument xml = new XmlDocument();
+            
             xml.Load("chelsea.xml");
-            dGVAttributes.DataSource = Chelsea;
 
+            dataGridView1.DataSource = Chelsea;
 
             foreach (XmlElement element in xml.DocumentElement)
             {
@@ -41,7 +43,81 @@ namespace IRF_Project
 
         private void buttonWrite_Click(object sender, EventArgs e)
         {
+            SaveFileDialog sfd = new SaveFileDialog();
 
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var c in Chelsea)
+                {
+                    sw.Write(c.Name);
+                    sw.Write(";");
+                    sw.Write(c.Country);
+                    sw.Write(";");
+                    sw.Write(c.BirthDate);
+                    sw.Write(";");
+                    sw.Write(c.Position);
+                    sw.Write(";");
+                    sw.Write(c.Goals);
+                    sw.WriteLine();
+                }
+            }
+        }
+
+        private void Reload_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = Chelsea;
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var player = new players();
+                Chelsea.Add(player);
+
+                player.Name = element.GetAttribute("name");
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                player.Country = childElement.GetAttribute("country");
+                player.BirthDate = childElement.GetAttribute("born");
+                player.Position = childElement.GetAttribute("position");
+                player.Goals = childElement.GetAttribute("goals");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = Chelsea;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = Chelsea;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = Chelsea;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = Chelsea;
+        }
+
+        private void btnAnimation_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Animation anim = new Animation();
+            anim.Show();
         }
     }
 }
